4ea0f9472750b1dff7fb7477776e704e24f07caf
Dhruv Patel <dhruvpatel704042@gmail.com>
2026-08-18 00:31:53 +0530

    Add files via upload

---
+++ PL3/PB1-A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number of marks: ";
+    cin >> n;
+
+    int marks[n];
+
+    cout << "Enter marks: ";
+    for (int i = 0; i < n; i++)
+        cin >> marks[i];
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (marks[j] > marks[j + 1])
+            {
+                int temp = marks[j];
+                marks[j] = marks[j + 1];
+                marks[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted marks: ";
+    for (int i = 0; i < n; i++)
+        cout << marks[i] << " ";
+
+    return 0;
+}
